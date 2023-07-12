4815962bff6da5e897bf8b716cf8511fd1aeda57
Tobi <136989523+TigredeLimon@users.noreply.github.com>
2023-07-12 17:26:10 +0200

    Acabado el slider
    --Hecho el pase de diapositivas en css y js (PERO A QUÉ PRECIO....)
    --Añadidos archivos .html para cada página

--- js/index.js
+++ js/index.js
@@ -12,24 +12,55 @@ estructura de este archivo .js :-)
 ----------------------------------------*/
 
 
-/* 
+/* -- PSEUDOCÓDIGO -- 
 Cuando hago CLICK en headerButton hace una FUNCTION
-A headerDropdown se le TOGGLE la clase isActive
-A headerArrow se le TOGGLE la clase isActive
-
-Cuando hago CLICK FUERA de headerButton hace una FUNCTION
-A headerDropdown se le QUITA/REMOVE la clase isActive
+    —> A headerDropdown se le TOGGLE la clase isActive
+    —> A headerArrow se le TOGGLE la clase isActive
 */
 
-// TODO: Resolver el tema de quitarle la class al dropdown al clickar fuera del item en sí
-const headerButton = document.querySelector(`.Header-button`)
+/*
+    ——> Función que añade y quita la clase isActive al menú desplegable
+    del header.
+*/
+const headerButton   = document.querySelector(`.Header-button`)
 const headerDropdown = document.querySelector(`.Header-dropdown`)
-const headerArrow = document.querySelector(`.Header-svg`)
+const headerArrow    = document.querySelector(`.Header-svg`)
 
 headerButton.addEventListener(`click`, ()=>{
     headerDropdown.classList.toggle(`isActive`)
     headerArrow.classList.toggle(`isActive`)
 })
 
+
+
+
+/* -- PSEUDOCÓDIGO -- 
+Cuando transcurrren 8 segundos, HeroGallery hace una FUNCTION
+    —> A galleryActive ++
+    —> Si... galleryActive es MAYOR O IGUAL (>=) a 3
+        —>galleryActive es IGUAL = 0
+
+    —>A TODAS las galleryImg se les REMOVE la clase isActive
+    —>A la galleryImg posicion galleryActive se le ADD la clase
+      isActive
+*/
+/*
+    ——> Función que activa un intervalo de 8 segundos, en el
+    cual se añade y/o se quita la clase isActive
+*/
+const galleryImg = document.querySelectorAll(`.Hero-gallery`)
+
+let galleryActive = 0
+
+setInterval(()=>{
+    galleryActive++
+    if(galleryActive >= 3){
+        galleryActive = 0}
+
+    galleryImg.forEach((eachImg, index)=>{
+        galleryImg[index].classList.remove(`isActive`)
+        galleryImg[galleryActive].classList.add(`isActive`)
+    })
+}, 8000)
 
 
