5a2a004fdfc75efc2badf76957273887ac89b033
Tobi <136989523+TigredeLimon@users.noreply.github.com>
2023-07-13 01:40:27 +0200

    Terminado footer
    --Acabado todo el footer
    --Revisar el hero, ¿tal vez otra composición iría mejor? Consultarlo con la almohada

--- js/index.js
+++ js/index.js
@@ -16,19 +16,33 @@ estructura de este archivo .js :-)
 Cuando hago CLICK en headerButton hace una FUNCTION
     —> A headerDropdown se le TOGGLE la clase isActive
     —> A headerArrow se le TOGGLE la clase isActive
+
+Cuando hago CLICK en htmlBody hace una FUNCTION 
+    —> A headerDropdown se le REMOVE la clase isActive
+    —> A headerArrow se le REMOVE la clase isActive 
 */
 
 /*
-    ——> Función que añade y quita la clase isActive al menú desplegable
-    del header.
+——> Función que añade y quita la clase isActive al menú desplegable
+del header
+——> Función que quita la clase isActive al clickar en el Body
+——> See: https://stackoverflow.com/questions/26672241/remove-class-after-click-outside-the-div
 */
 const headerButton   = document.querySelector(`.Header-button`)
 const headerDropdown = document.querySelector(`.Header-dropdown`)
 const headerArrow    = document.querySelector(`.Header-svg`)
+const htmlBody = document.querySelector(`body`)
 
-headerButton.addEventListener(`click`, ()=>{
+headerButton.addEventListener(`click`, (e)=>{
     headerDropdown.classList.toggle(`isActive`)
     headerArrow.classList.toggle(`isActive`)
+    e.stopPropagation()
+})
+
+htmlBody.addEventListener(`click`, ()=>{
+    if(headerDropdown.classList.contains(`isActive`) && headerArrow.classList.contains(`isActive`))
+    {headerDropdown.classList.remove(`isActive`)
+    headerArrow.classList.remove(`isActive`)}
 })
 
 
@@ -48,7 +62,7 @@ Cuando transcurrren 8 segundos, HeroGallery hace una FUNCTION
     ——> Función que activa un intervalo de 8 segundos, en el
     cual se añade y/o se quita la clase isActive
 */
-const galleryImg = document.querySelectorAll(`.Hero-gallery`)
+const galleryImg = document.querySelectorAll(`.Hero-img`)
 
 let galleryActive = 0
 
